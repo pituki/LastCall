300ba7c9ea78de65009c0dca6dc80893c08af17d
pituki <pituki@pitukis-macbook-pro.local>
2014-09-16 00:13:13 -0400

    added Karma coverage and karma jasmine with reports.

---
+++ karma.conf.js
@@ -0,0 +1,74 @@
+// Karma configuration
+// Generated on Wed Aug 27 2014 21:23:28 GMT-0700 (PDT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '.',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'LastCall.js',
+      'tests/*Spec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        'LastCall.js':["coverage"]
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress','dots','junit','coverage'],
+
+  junitReporter: {
+      // will be resolved to basePath (in the same way as files/exclude patterns)
+      outputFile: './reports/test-results.xml'
+  },
+    coverageReporter: {
+        type : 'html',
+        dir : './reports/'
+    },
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS', 'Chrome'], //, 'Firefox'
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
